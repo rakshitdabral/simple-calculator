bed40af99145b97b42d731f24350b2718f5fd2bf
Rakshit Dabral <96056758+rakshitdabral@users.noreply.github.com>
2022-01-10 13:49:05 +0530

    calculator

--- calculator.cpp
+++ calculator.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main(){
-        
+    int n;
+        cout << "Enter size of array: ";
+        cin >> n;
+        int arr[n];
+        cout << "Enter numbers: ";
+        for(int i=0;i<n;i++){
+            cin >> arr[i];
+        }
+        cout << arr << endl; 
     return 0;
 }
